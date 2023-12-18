1e079892f8ac3b1b5b9cd392137e342dad5a952b
mfedorchuk <maksym.fedorchuk@corp.kismia.com>
2023-12-18 21:29:59 +0200

    GF-6: run auth flow

--- main.py
+++ main.py
@@ -1,41 +1,41 @@
 import functions_framework
 
-from google.cloud import bigquery
+# from google.cloud import bigquery
 from datetime import datetime
 
-from dotenv.main import dotenv_values
+# from dotenv.main import dotenv_values
 
-config = dotenv_values(".env")
+# config = dotenv_values(".env")
 
 
-def record_data(raw: dict) -> tuple:
-    try:
-        # Init BigQuery
-        client = bigquery.Client()
-
-        # Get BigQuery table link
-        dataset_ref = config["DATASET"]
-        table_ref = dataset_ref.table(config["TABLE"])
-        table = client.get_table(table_ref)
-
-        # Data for table
-        rows_to_insert = [
-            {'dt': datetime.utcnow(), 'location': 'home', 'sensor_id': 1, 'scale': 'celsius', 'temperature': 0}
-        ]
-
-        # Insert rows
-        errors = client.insert_rows(table, rows_to_insert)
-
-        if errors:
-            raise Exception(f'Error inserting rows: {errors}')
-
-        # Ok. 200
-        return 'Data successfully written to BigQuery.', 200
-
-    except Exception as e:
-        print(f'Error: {e}')
-        # Обробка помилок та відповідь на невдале виконання
-        return 'Error writing data to BigQuery.', 500
+# def record_data(raw: dict) -> tuple:
+#     try:
+#         # Init BigQuery
+#         client = bigquery.Client()
+#
+#         # Get BigQuery table link
+#         dataset_ref = config["DATASET"]
+#         table_ref = dataset_ref.table(config["TABLE"])
+#         table = client.get_table(table_ref)
+#
+#         # Data for table
+#         rows_to_insert = [
+#             {'dt': datetime.utcnow(), 'location': 'home', 'sensor_id': 1, 'scale': 'celsius', 'temperature': 0}
+#         ]
+#
+#         # Insert rows
+#         errors = client.insert_rows(table, rows_to_insert)
+#
+#         if errors:
+#             raise Exception(f'Error inserting rows: {errors}')
+#
+#         # Ok. 200
+#         return 'Data successfully written to BigQuery.', 200
+#
+#     except Exception as e:
+#         print(f'Error: {e}')
+#         # Обробка помилок та відповідь на невдале виконання
+#         return 'Error writing data to BigQuery.', 500
 
 
 @functions_framework.http
